26f155fd8a6b60de4a65e067bb55f70d4dd91ee4
dyseg <bttf87@gmail.com>
2025-06-21 08:37:34 +0200

    bugfix: print importance correctly on WebUI

--- src/webui/www/private/scripts/dynamicTable.js
+++ src/webui/www/private/scripts/dynamicTable.js
@@ -1437,7 +1437,7 @@ window.qBittorrent.DynamicTable ??= (() => {
             };
 
             // importance
-            this.columns["importance".updateId] = function(td, row) {
+            this.columns["importance"].updateTd = function(td, row) {
                 const value = this.getRowValue(row);
                 const importance = (value === -1) ? "∞" : window.qBittorrent.Misc.toFixedPointString(value, 2);
                 td.textContent = importance;
